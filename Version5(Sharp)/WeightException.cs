1cb10bddec881e61e3a455010424b159030195ae
Vladyslav Ishchenko <vladik.ishchenko.2017@gmail.com>
2018-01-18 13:39:18 +0200

    Class for processing exceptions related to wrong entered user weight

---
+++ Version5(Sharp)/WeightException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Version5
+{
+    /*!
+\brief Клас відповідає за обробку виключень пов'язаних з вагою користувача
+\author Ishchenko Vladyslav
+\version 5
+\date 15 грудня 2017
+*/
+    class WeightException:Exception
+    {
+        /// <summary>
+        /// Конструктор ініціалізації
+        /// </summary>
+        /// <param name="message">Повідомлення про помилку</param>
+        public WeightException(string message):
+            base(message)
+        {
+
+        }
+    }
+}
