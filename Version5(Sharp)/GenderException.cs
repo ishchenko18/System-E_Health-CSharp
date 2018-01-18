2d6778f42d09831f007a57cff52cb860ff37e054
Vladyslav Ishchenko <vladik.ishchenko.2017@gmail.com>
2018-01-18 13:34:49 +0200

    Class for processing exceptions related to wrong entered gender

---
+++ Version5(Sharp)/GenderException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Version5
+{
+    /*!
+\brief Клас відповідає за обробку виключень пов'язаних зі статтю користувача
+\author Ishchenko Vladyslav
+\version 5
+\date 15 грудня 2017
+*/
+    class GenderException:Exception
+    {
+        /// <summary>
+        /// Конструктор ініціалізації
+        /// </summary>
+        /// <param name="message">Повідомлення про помилку</param>
+        public GenderException(string message):
+            base(message)
+        {
+
+        }
+    }
+}
