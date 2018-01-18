aadc490f3d04c1b8aa6c4747624f6058c7bb5a14
Vladyslav Ishchenko <vladik.ishchenko.2017@gmail.com>
2018-01-18 13:36:29 +0200

    Class for processing exceptions related to wrong entered group of blood

---
+++ Version5(Sharp)/GroupOfBloodException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Version5
+{
+    /*!
+\brief Клас відповідає за обробку виключень пов'язаних з групою крові користувача
+\author Ishchenko Vladyslav
+\version 5
+\date 15 грудня 2017
+*/
+    class GroupOfBloodException:Exception
+    {
+        /// <summary>
+        /// Конструктор ініціалізації
+        /// </summary>
+        /// <param name="message">Повідомлення про помилку</param>
+        public GroupOfBloodException(string message):
+            base(message)
+        {
+            
+        }
+    }
+}
