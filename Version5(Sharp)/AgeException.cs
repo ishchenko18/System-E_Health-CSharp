c2a03ed39a2fbcf5f90134dca6a6e52ae611869c
Vladyslav Ishchenko <vladik.ishchenko.2017@gmail.com>
2018-01-18 13:12:40 +0200

    Class for processing exception, when entered not correct age

---
+++ Version5(Sharp)/AgeException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Version5
+{
+ /*!
+\brief Клас відповідає за обробку виключень пов'язаних з віком користувача
+\author Ishchenko Vladyslav
+\version 5
+\date 15 грудня 2017
+*/
+    class AgeException:Exception
+    {
+        /// <summary>
+        /// Конструктор ініціалізації
+        /// </summary>
+        /// <param name="message">Повідомлення про помилку</param>
+        public AgeException(string message):
+            base(message)
+        {
+
+        }
+    }
+}
