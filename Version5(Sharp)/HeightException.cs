39a3a9b759f0923ad621cee66615fa83d7c505c5
Vladyslav Ishchenko <vladik.ishchenko.2017@gmail.com>
2018-01-18 13:36:59 +0200

    Class for processing exceptions related to wrong entered user height

---
+++ Version5(Sharp)/HeightException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Version5
+{
+    /*!
+\brief Клас відповідає за обробку виключень пов'язаних зі зростом користувача
+\author Ishchenko Vladyslav
+\version 5
+\date 15 грудня 2017
+*/
+    class HeightException:Exception
+    {
+        /// <summary>
+        /// Конструктор ініціалізації
+        /// </summary>
+        /// <param name="message">Повідомлення про помилку</param>
+        public HeightException(string message):
+            base(message)
+        {
+
+        }
+    }
+}
